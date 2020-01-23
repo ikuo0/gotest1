aece4323d44e2f1b6a41d907a75b253f3741fdeb
ikuo hayasaka <ikuo.pg@gmail.com>
2020-01-23 20:27:57 +0900

    any way

--- numlib/numlib.go
+++ numlib/numlib.go
@@ -2,8 +2,11 @@
 package numlib
 
 import (
+    //"fmt"
+    "io/ioutil"
     "log"
     "os"
+    "regexp"
     "strconv"
     "strings"
     //n1d "github.com/ikuo0/gotest1/num1d"
@@ -25,6 +28,47 @@ func SaveTxt(fileName string, m n2d.Mat) {
                 sarr = append(sarr, strconv.FormatFloat(m[r][c], 'e', -1, 64))
             }
             f.Write([]byte(strings.Join(sarr, "\t") + "\n"))
+        }
+    }
+}
+
+func LoadTxt(fileName string) (bool, n2d.Mat){
+    if f, err := os.Open(fileName); err != nil {
+        log.Fatal(err)
+        return false, nil
+    } else {
+        defer func() {
+            f.Close()
+        }()
+        if b, err := ioutil.ReadAll(f); err != nil {
+            log.Fatal(err)
+            return false, nil
+        } else {
+            s := string(b)
+            repChomp := regexp.MustCompile("[\r\n]+$")
+            s = repChomp.ReplaceAllString(s, "")
+            rep := regexp.MustCompile("[\r\n]+")
+            lines := rep.Split(s, -1)
+            rSize := len(lines)
+            if rSize < 1 {
+                log.Fatal("row length 0")
+                return false, nil
+            } else {
+                firstCol := strings.Split(lines[0], "\t")
+                cSize := len(firstCol)
+                m := n2d.Create(rSize, cSize)
+                for ri, l := range(lines) {
+                    for ci, c := range(strings.Split(l, "\t")) {
+                        if f, err := strconv.ParseFloat(c, 64); err != nil {
+                            log.Fatal(err)
+                            return false, nil
+                        } else {
+                            m[ri][ci] = f
+                        }
+                    }
+                }
+                return true, m
+            }
         }
     }
 }
--- numlib/numlib_test.go
+++ numlib/numlib_test.go
@@ -2,6 +2,8 @@
 package numlib
 
 import (
+	"fmt"
+	"log"
 	"testing"
 	n2d "github.com/ikuo0/gotest1/num2d"
 )
@@ -10,3 +12,12 @@ func Test1(t* testing.T) {
 	m := n2d.Opt().RandomType(n2d.ConstRandomUniform).Rand(100, 4)
 	SaveTxt("./test.txt", m)
 }
+
+func Test2(t* testing.T) {
+	if ok, m := LoadTxt("./test.txt"); ok == false {
+		log.Fatal("LoadTxt error")
+	} else {
+		fmt.Println(m)
+	}
+}
+
